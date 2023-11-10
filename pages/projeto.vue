6600c2e3c1150cdfa220b2e9d621a5fcf2f7d3fa
MattheusAlfaia <81316219+leandro-rgb@users.noreply.github.com>
2023-11-10 15:14:15 -0400

    Página Projeto
    Fiz o Template/Cenário onde o robô irá andar.

---
+++ pages/projeto.vue
@@ -0,0 +1,18 @@
+<template>
+    <h1>Agente Racional</h1>
+    <div class="conffeti">
+        <!-- confete -->
+    </div>
+    <button>Iniciar Limpeza</button>
+    <div class="container">
+        <div class="robopos"></div>
+        <div class="ambiente">
+            <div class="localizacao"></div>
+        </div>
+        <div class="infos">
+            <p>🔋: 100%</p>
+            <p>🧹</p>
+            <p>🎒: 1</p>
+        </div>
+    </div>
+</template>
